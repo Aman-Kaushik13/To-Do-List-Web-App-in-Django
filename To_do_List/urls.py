21ab652e9a7eb01c541cdab4f0e6864a38782db2
Aman-Kaushik13 <64030485+Aman-Kaushik13@users.noreply.github.com>
2020-07-06 19:23:02 +0530

    Delete urls.py

--- To_do_List/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from To_do_App import views
-urlpatterns = [
-    
-    path('',views.home),
-    path('add_todo/',views.add_todo),
-    path('delete_todo/<int:todo_id>/',views.delete_todo),
-]
